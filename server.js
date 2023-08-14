619fa54356ab05c6ab330f1b8c2123476bb0c1de
Sundram Agarwal <agarwal.sundaram@gmail.com>
2023-08-14 16:32:52 +0530

    server.js commit

--- server.js
+++ server.js
@@ -1,1 +1,37 @@
-const express = require("express")
+const dotenv = require("dotenv").config();
+const express = require("express");
+const mongoose = require("mongoose");
+mongoose.set("strictQuery", false);
+const bodyParser = require("body-parser");
+const cors = require("cors");
+const morgan = require("morgan")
+const colors = require("colors")
+
+const app = express()
+app.use(
+    cors({
+      origin: [
+        "http://localhost:3000",
+        "https://personal-gpt.vercel.app",
+      ],
+      credentials: true,
+    })
+  );
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+app.use(morgan('dev'))
+
+//connect to DB and start server
+const PORT = process.env.PORT || 5000;
+mongoose
+  .connect(process.env.MONGO_URI)
+  .then(() => {
+    app.listen(PORT, () => {
+      console.log(`Server Running on PORT ${PORT} in ${process.env.NODE_ENV} mode`.bgCyan);
+    });
+  })
+  .catch((err) => {
+    console.log(err);
+  });
